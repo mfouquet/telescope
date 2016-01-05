258a97451b6a22340b1132e8bf1518821c7c1a7d
Michael Fouquet <michael.fouquet@hudl.com>
2016-01-05 10:32:56 -0600

    Initial commit of code

---
+++ site.js
@@ -0,0 +1,25 @@
+
+
+var url = "https://api.nasa.gov/planetary/apod?api_key=" + key + "&format=JSON";
+
+
+$.ajax({
+  url: url,
+  success: handleResult
+});
+
+function handleResult(result){
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "none");
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "none");
+    $("#image_obj").attr("src", result.url);
+  }
+
+  //$("#reqObject").text(url);
+  $("#returnObject").text(JSON.stringify(result, null, 4));
+  $("#apod_explaination").text(result.explanation);
+  $("#header_obj").text(result.title);
+}
