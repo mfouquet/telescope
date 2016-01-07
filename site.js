fd14a089fc2e6352e754d9edf8eae4f0e9ad1cbb
Michael Fouquet <michael.fouquet@hudl.com>
2016-01-07 12:48:27 -0600

    Generate a random number for failed api call

--- site.js
+++ site.js
@@ -9,17 +9,22 @@ $.ajax({
 });
 
 function handleResult(result){
-  if(result.media_type == "video") {
-    $("#apod_img_id").css("display", "none");
-    $("#apod_vid_id").attr("src", result.url);
-  }
-  else {
-    $("#apod_vid_id").css("display", "none");
-    $("#image_obj").attr("src", result.url);
-  }
+  var randomNumber = Math.floor(Math.random() * 10) + 1;
 
-  //$("#reqObject").text(url);
-  $("#returnObject").text(JSON.stringify(result, null, 4));
-  $("#apod_explaination").text(result.explanation);
-  $("#header_obj").text(result.title);
+
+  $("#image_obj").attr("src", "images/" + randomNumber.toString() + ".jpg");
+
+  // if(result.media_type == "video") {
+  //   $("#apod_img_id").css("display", "none");
+  //   $("#apod_vid_id").attr("src", result.url);
+  // }
+  // else {
+  //   $("#apod_vid_id").css("display", "none");
+  //   $("#image_obj").attr("src", result.url);
+  // }
+  //
+  // //$("#reqObject").text(url);
+  // $("#returnObject").text(JSON.stringify(result, null, 4));
+  // $("#apod_explaination").text(result.explanation);
+  // $("#header_obj").text(result.title);
 }
