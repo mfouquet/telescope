480b05f245628c696312b8fa4784a9a4a6bcae0e
Michael Fouquet <michael.fouquet@hudl.com>
2016-01-24 22:19:08 -0600

    Add in a loading animation

--- site.js
+++ site.js
@@ -14,17 +14,18 @@ function handleResult(result){
 
   $("#image_obj").attr("src", "images/" + randomNumber.toString() + ".jpg");
 
-  // if(result.media_type == "video") {
-  //   $("#apod_img_id").css("display", "none");
-  //   $("#apod_vid_id").attr("src", result.url);
-  // }
-  // else {
-  //   $("#apod_vid_id").css("display", "none");
-  //   $("#image_obj").attr("src", result.url);
-  // }
-  //
-  // //$("#reqObject").text(url);
-  // $("#returnObject").text(JSON.stringify(result, null, 4));
-  // $("#apod_explaination").text(result.explanation);
-  // $("#header_obj").text(result.title);
+  if(result.media_type == "video") {
+    $("#apod_img_id").css("display", "none");
+    $("#apod_vid_id").attr("src", result.url);
+  }
+  else {
+    $("#apod_vid_id").css("display", "none");
+    $("#image_obj").attr("src", result.url);
+    $(".image__container").addClass("image__container--shown");
+  }
+
+  //$("#reqObject").text(url);
+  $("#returnObject").text(JSON.stringify(result, null, 4));
+  $("#apod_explaination").text(result.explanation);
+  $("#header_obj").text(result.title);
 }
